9c810b25d7de69cd0a0bbb4be80834941e9dc97c
genuinelycool <genuinelycool@gmail.com>
2025-03-06 19:34:28 +0545

    156. multi auth working with admin authentication system part 6

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
+            'auth' => Authenticate::class,
             'guest' => RedirectIfAuthenticated::class
         ]);
     })
