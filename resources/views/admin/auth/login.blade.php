3364ea7b3af44733b061747de20d7e37cbb45756
genuinelycool <genuinelycool@gmail.com>
2025-03-08 02:32:54 +0545

    171. admin - admin reset page mastering

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -35,6 +35,10 @@
                         class="navbar-brand-image">
                 </a>
             </div>
+            
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4" :status="session('status')" />
+
             <div class="card card-md">
                 <div class="card-body">
                     <h2 class="h2 text-center mb-4">{{ __('Login to your account') }}</h2>
@@ -53,30 +57,13 @@
                             <label class="form-label">
                                 {{ __('Password') }}
                                 <span class="form-label-description">
-                                    <a href="./forgot-password.html">{{ __('I forgot password') }}</a>
+                                    <a href="{{ route('admin.password.request') }}">{{ __('I forgot password') }}</a>
                                 </span>
                             </label>
                             <div class="input-group input-group-flat">
                                 <input type="password" name="password" required autocomplete="current-password"
                                     class="form-control" placeholder="Your password">
-                                <span class="input-group-text">
-                                    @if (Route::has('password.request'))
-                                        <a href="{{ route('admin.password.request') }}" class="link-secondary"
-                                            title="Show password"
-                                            data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24"
-                                                height="24" viewBox="0 0 24 24" stroke-width="2"
-                                                stroke="currentColor" fill="none" stroke-linecap="round"
-                                                stroke-linejoin="round">
-                                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-                                                <path
-                                                    d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
-                                            </svg>
-                                        </a>
-                                    @endif
 
-                                </span>
                             </div>
                         </div>
                         <div class="mb-2">
