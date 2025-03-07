1e8f3ebb8056854db2583b2fc88e55ea462ffae1
genuinelycool <genuinelycool@gmail.com>
2025-03-07 23:17:38 +0545

    169. frontend - register page mastering

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,51 +1,3 @@
-{{-- <x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :message="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :message="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout> --}}
-
 @extends('frontend.layouts.master')
 
 @section('content')
@@ -61,17 +13,17 @@
 
                             <ul class="breadcrumb-list flx-align gap-2 mb-2 justify-content-center">
                                 <li class="breadcrumb-list__item font-14 text-body">
-                                    <a href="index.html" class="breadcrumb-list__link text-body hover-text-main">Home</a>
+                                    <a href="index.html" class="breadcrumb-list__link text-body hover-text-main">{{ __('Home') }}</a>
                                 </li>
                                 <li class="breadcrumb-list__item font-14 text-body">
                                     <span class="breadcrumb-list__icon font-10"><i class="fas fa-chevron-right"></i></span>
                                 </li>
                                 <li class="breadcrumb-list__item font-14 text-body">
-                                    <span class="breadcrumb-list__text">Sign In</span>
+                                    <span class="breadcrumb-list__text">{{ __('Sign In') }}</span>
                                 </li>
                             </ul>
 
-                            <h3 class="breadcrumb-two-content__title mb-0 text-capitalize">Sign In</h3>
+                            <h3 class="breadcrumb-two-content__title mb-0 text-capitalize">{{ __('Sign In') }}</h3>
                         </div>
                     </div>
                 </div>
@@ -85,15 +37,15 @@
             <div class="row">
                 <div class="col-xxl-5 col-xl-6 col-md-9 col-lg-7 m-auto">
                     <div class="wsus__login_area">
-                        <h2>Welcome back!</h2>
-                        <p>sign in to continue</p>
+                        <h2>{{ __('Welcome back!') }}</h2>
+                        <p>{{ __('sign in to continue') }}</p>
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
 
                             <div class="row">
                                 <div class="col-xl-12">
                                     <div class="wsus__login_imput">
-                                        <label>email</label>
+                                        <label>{{ __('email') }}</label>
                                         <input type="email" name="email" value="{{ old('email') }}" placeholder="Email"
                                             required>
                                         <x-input-error :message="$errors->first('email')" class="mt-2" />
@@ -101,7 +53,7 @@
                                 </div>
                                 <div class="col-xl-12">
                                     <div class="wsus__login_imput">
-                                        <label>password</label>
+                                        <label>{{ __('password') }}</label>
                                         <input type="password" name="password" required placeholder="Password">
                                         <x-input-error :message="$errors->first('password')" class="mt-2" />
                                     </div>
@@ -112,21 +64,21 @@
                                             <input class="form-check-input" type="checkbox" value=""
                                                 id="flexCheckDefault" name="remember">
                                             <label class="form-check-label" for="flexCheckDefault">
-                                                Remember Me
+                                                {{ __('Remember Me') }}
                                             </label>
                                         </div>
-                                        <a href="{{ route('password.request') }}">Forget Password ?</a>
+                                        <a href="{{ route('password.request') }}">{{ __('Forget Password') }} ?</a>
                                     </div>
                                 </div>
                                 <div class="col-xl-12">
                                     <div class="wsus__login_imput">
-                                        <button type="submit" class="btn btn-main btn-lg">Sign in</button>
+                                        <button type="submit" class="btn btn-main btn-lg">{{ __('Sign in') }}</button>
                                     </div>
                                 </div>
                             </div>
                         </form>
 
-                        <p class="create_account">Don't have an account ? <a href="{{ route('register') }}">Sign Up</a></p>
+                        <p class="create_account">{{ __('Dont have an account') }} ? <a href="{{ route('register') }}">{{ __('Sign Up') }}</a></p>
                     </div>
                 </div>
             </div>
--- resources/views/frontend/dashboard/layouts/master.blade.php
+++ resources/views/frontend/dashboard/layouts/master.blade.php
@@ -160,72 +160,7 @@
         <div class="dashboard__inner d-flex">
 
             <!-- ===================== Dashboard Sidebar Start ======================= -->
-            <div class="dashboard-sidebar">
-                <button type="button" class="dashboard-sidebar__close d-lg-none d-flex"><i
-                        class="las la-times"></i></button>
-                <div class="dashboard-sidebar__inner">
-                    <a href="index.html" class="logo mb-48">
-                        <img src="assets/images/logo/logo.png" alt="" class="white-version">
-                    </a>
-                    <a href="index.html" class="logo logo_icon favicon mb-48">
-                        <img src="assets/images/thumbs/dashboard_sidebar_icon.png" alt="">
-                    </a>
-
-                    <!-- Sidebar List Start -->
-                    <ul class="sidebar-list">
-                        <li class="sidebar-list__item">
-                            <a href="dashboard.html" class="sidebar-list__link">
-                                <span class="sidebar-list__icon">
-                                    <i class="ti ti-device-heart-monitor"></i>
-                                </span>
-                                <span class="text">Dashboard</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-list__item">
-                            <a href="dashboard-profile.html" class="sidebar-list__link">
-                                <span class="sidebar-list__icon">
-                                    <i class="ti ti-user"></i>
-                                </span>
-                                <span class="text">Profile</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-list__item">
-                            <a href="setting.html" class="sidebar-list__link">
-                                <span class="sidebar-list__icon">
-                                    <i class="ti ti-settings"></i>
-                                </span>
-                                <span class="text">Settings</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-list__item">
-                            <a href="dashboard-table.html" class="sidebar-list__link">
-                                <span class="sidebar-list__icon">
-                                    <i class="ti ti-list-details"></i>
-                                </span>
-                                <span class="text">Table Design</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-list__item">
-                            <a href="dashboard-form.html" class="sidebar-list__link">
-                                <span class="sidebar-list__icon">
-                                    <i class="ti ti-list"></i>
-                                </span>
-                                <span class="text">Form Design</span>
-                            </a>
-                        </li>
-                        <li class="sidebar-list__item">
-                            <a href="login.html" class="sidebar-list__link">
-                                <span class="sidebar-list__icon">
-                                    <i class="ti ti-logout"></i>
-                                </span>
-                                <span class="text">Logout</span>
-                            </a>
-                        </li>
-                    </ul>
-                    <!-- Sidebar List End -->
-
-                </div>
-            </div>
+            @include('frontend.dashboard.layouts.sidebar')
             <!-- ===================== Dashboard Sidebar End ======================= -->
 
             <div class="dashboard-body">
@@ -323,11 +258,7 @@
                 <div class="dashboard-footer bottom-footer-two mt-32 border-0 bg-white">
                     <div class="bottom-footer__inner flx-between gap-3">
                         <p class="bottom-footer__text font-14"> Copyright © 2024 DigiMart, All rights reserved.</p>
-                        <div class="footer-links gap-4">
-                            <a href="#" class="footer-link hover-text-heading font-14">Terms of service</a>
-                            <a href="#" class="footer-link hover-text-heading font-14">Privacy Policy</a>
-                            <a href="#" class="footer-link hover-text-heading font-14">cookies</a>
-                        </div>
+                        
                     </div>
                 </div>
                 <!-- ====================== Dashboard Footer End ======================== -->
