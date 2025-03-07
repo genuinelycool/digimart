35628b0405e37df5aa488dd0d5f45d6c3002662e
genuinelycool <genuinelycool@gmail.com>
2025-03-07 17:47:29 +0545

    168. frontend - dashboard page mastering

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
+use App\Http\Controllers\Frontend\DashboardController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+});
 
 
 require __DIR__.'/auth.php';
