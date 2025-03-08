da4a3bede456896e7560567a8e28f95035cbfdb6
genuinelycool <genuinelycool@gmail.com>
2025-03-08 14:37:20 +0545

    175. profile update - update working with profile update part 1

--- resources/views/frontend/dashboard/layouts/sidebar.blade.php
+++ resources/views/frontend/dashboard/layouts/sidebar.blade.php
@@ -19,7 +19,7 @@
                 </a>
             </li>
             <li class="sidebar-list__item">
-                <a href="dashboard-profile.html" class="sidebar-list__link">
+                <a href="{{ route('profile') }}" class="sidebar-list__link">
                     <span class="sidebar-list__icon">
                         <i class="ti ti-user"></i>
                     </span>
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,15 @@
 
 use App\Http\Controllers\Frontend\DashboardController;
 use App\Http\Controllers\Frontend\HomeController;
+use App\Http\Controllers\Frontend\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::group(['middleware' => ['auth', 'verified']], function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::get('profile', [ProfileController::class, 'index'])->name('profile');
 });
 
 
