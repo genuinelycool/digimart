ed56072cdf5cfc2d928a8df6127d58809db25a04
genuinelycool <genuinelycool@gmail.com>
2025-03-17 21:11:48 +0545

    214. kyc verification - working with kyc verification part 3

--- app/Http/Controllers/Frontend/KycVerificationController.php
+++ app/Http/Controllers/Frontend/KycVerificationController.php
@@ -5,12 +5,18 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Frontend\KycVerificationStoreRequest;
 use App\Models\KycSetting;
+use App\Models\KycVerification;
+use App\Services\NotificationService;
+use App\Traits\FileUpload;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class KycVerificationController extends Controller
 {
+    use FileUpload;
+
     function index() : View
     {
         $kycSetting = KycSetting::first();
@@ -19,6 +25,23 @@ class KycVerificationController extends Controller
 
     function store(KycVerificationStoreRequest $request) : RedirectResponse
     {
-        dd($request->all());
+        $kyc = new KycVerification();
+        $kyc->user_id = Auth::guard('web')->user()->id;
+        $kyc->document_type = $request->document_type;
+        $kyc->document_number = $request->document_number;
+
+        if($request->hasFile('documents')) {
+            $paths = [];
+            foreach($request->file('documents') as $file) {
+                $paths[] = $this->uploadFile($file, disk:'local');
+            }
+        }
+
+        $kyc->documents = json_encode($paths);
+        $kyc->save();
+
+        NotificationService::CREATED(__('Your KYC verification has been submitted successfully'));
+
+        return to_route('dashboard');
     }
 }
