b23e4284d18a692b7e76490d16a0aa7684ae5614
genuinelycool <genuinelycool@gmail.com>
2025-03-26 23:24:03 +0545

    247. product module - working with item module part 5

--- resources/views/frontend/dashboard/item/create.blade.php
+++ resources/views/frontend/dashboard/item/create.blade.php
@@ -42,16 +42,17 @@
             <hr>
             <div class="row">
                 <div class="col-md-12">
-                    <x-frontend.input-select name="category" :label="__('Category')" :required="true" disabled >
+                    <x-frontend.input-select name="category" :label="__('Category')" :required="true" disabled>
                         @foreach ($categories as $categoryItem)
-                        <option @selected($selectedCategory->slug == $categoryItem->slug) value="{{ $categoryItem->slug }}">{{ $categoryItem->name }}</option>
+                            <option @selected($selectedCategory->slug == $categoryItem->slug) value="{{ $categoryItem->slug }}">{{ $categoryItem->name }}
+                            </option>
                         @endforeach
                     </x-frontend.input-select>
                 </div>
                 <div class="col-md-12">
-                    <x-frontend.input-select name="sub_category" class="select_2" :label="__('Sub Category')" :required="true" >
+                    <x-frontend.input-select name="sub_category" class="select_2" :label="__('Sub Category')" :required="true">
                         @foreach ($selectedCategory->subCategories as $subCategory)
-                        <option value="{{ $subCategory->slug }}">{{ $subCategory->name }}</option>
+                            <option value="{{ $subCategory->slug }}">{{ $subCategory->name }}</option>
                         @endforeach
                     </x-frontend.input-select>
                 </div>
@@ -69,6 +70,52 @@
                     <br>
                     <input type="text" value="" data-role="tagsinput" />
                 </div>
+            </div>
+        </div>
+
+        <div class="wsus__dash_order_table mt-3">
+            <div>
+                <h6>{{ __('Files') }}</h6>
+            </div>
+
+            <hr>
+            <div class="row">
+
+                <div class="col-md-12">
+                    <x-frontend.input-select name="preview_type" class="" :label="__('Preview Type')" :required="true">
+                        <option value="image">{{ __('Image') }}</option>
+                        <option value="video">{{ __('Video') }}</option>
+                        <option value="audio">{{ __('Audio') }}</option>
+                    </x-frontend.input-select>
+                </div>
+
+                <div class="col-md-12">
+                    <x-frontend.input-select name="preview_file" class="" :label="__('Preview File')" :required="true">
+
+                    </x-frontend.input-select>
+                </div>
+                <div class="col-md-12">
+                    <label for="" class="form-label mb-2 font-18 font-heading fw-600">Main File <span
+                            class="text-danger">*</span></label>
+                    <div class="input-group mb-3">
+                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">Dropdown</button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                        </ul>
+                        <input type="text" class="form-control" aria-label="Text input with dropdown button">
+                    </div>
+                </div>
+
+                <div class="col-md-12">
+                    <x-frontend.input-select name="screenshots[]" class="select_2" :label="__('Screenshots')" multiple="multiple">
+                        <option value="">test 1</option>
+                        <option value="">test 2</option>
+                        <option value="">test 3</option>
+                    </x-frontend.input-select>
+                </div>
+
             </div>
         </div>
     </form>
